851c2800da8e92828522c8733bc32a459a79bbee
Nicholas Martin <nicholas.martin@tharsus.co.uk>
2020-09-08 15:43:53 +0100

    get axios to work to grab data from the server

--- T-AI/hackathon.py
+++ T-AI/hackathon.py
@@ -5,12 +5,34 @@ import os
 import flask
 from flask import Flask, request, render_template, send_from_directory
 from flask import jsonify
-data = {
+data = [{
     "uploadTime": "18:36",
     "id": "3",
-    "GPS": 3,
+    "long": -122.445368,
+    "lat": 37.782551,
     "pollution": 4
-}
+},
+{
+    "uploadTime": "18:36",
+    "id": "3",
+    "long": -122.444586,
+    "lat": 37.782745,
+    "pollution": 4
+},{
+    "uploadTime": "18:36",
+    "id": "3",
+    "long": -122.443688,
+    "lat": 37.782842,
+    "pollution": 4
+},{
+    "uploadTime": "18:36",
+    "id": "3",
+    "long": -122.442815,
+    "lat": 37.782919,
+    "pollution": 4
+},
+]
+
 p = os.path.dirname(os.path.realpath(__file__))
 p = os.path.join(p, "index.html")
 
